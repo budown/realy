2f1ff154e9792f3f3796e2b693d4fa4ec819bc2c
kzz <kzz45@qq.com>
2023-06-09 19:00:18 +0800

    README

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,17 @@
 module.exports = {
-    title: 'xxx',
-    description: 'xxx',
+    title: '东寨村',
+    description: '东寨村',
+    themeConfig: {
+        subSidebar: "auto",
+        colorModeSwitch: "auto",
+        nav: [
+            { text: "首页", link: "/" },
+        ],
+        sidebar: [
+            {
+                title: "第一",
+                path: "/",
+            }
+        ]
+    }
 }
